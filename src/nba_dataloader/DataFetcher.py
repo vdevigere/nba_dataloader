db3367a46a019497cb4f714cb1b9d3cf3ed11796
vdevigere <vdevigere+git@gmail.com>
2023-11-08 13:35:22 -0800

    Saving the raw response to the file system. Shutting down ray right after the query operations Removing the commented out season params Using pathlib.Path for better compatibility.

--- src/nba_dataloader/DataFetcher.py
+++ src/nba_dataloader/DataFetcher.py
@@ -2,10 +2,23 @@ import pandas
 import pandas as pd
 import ray
 import requests
+from typing import Dict, Any
+import hashlib
+import json
+
+
+def dict_hash(dictionary: Dict[str, Any]) -> str:
+    """MD5 hash of a dictionary."""
+    dhash = hashlib.md5()
+    # We need to sort arguments so {'a': 1, 'b': 2} is
+    # the same as {'b': 2, 'a': 1}
+    encoded = json.dumps(dictionary, sort_keys=True).encode()
+    dhash.update(encoded)
+    return dhash.hexdigest()
 
 
 # Generic helper function to query stats.nba.com endpoint and fetch details.
-def fetch_data(endpoint, params: dict):
+def fetch_data(endpoint, params: dict) -> Dict:
     reqHeaders = {
         'User-Agent': 'PythonScript',
         'Referer': 'https://www.nba.com/',
@@ -14,13 +27,13 @@ def fetch_data(endpoint, params: dict):
 
     url = f"https://stats.nba.com/stats/{endpoint}/"
     resp = requests.get(url, headers=reqHeaders, params=params)
-    # print(f"URL:{resp.url}")
-    retVal = {'STATUS': resp.status_code}
+    retVal = {'STATUS': resp.status_code, 'RAW': resp.text}
 
     if resp.status_code == 200:
-        json = resp.json()
-        reqParams = {k: v for k, v in json['parameters'].items() if v is not None}
-        result_sets = json['resultSets']
+        jsonResp = resp.json()
+        reqParams = {k: v for k, v in jsonResp['parameters'].items() if v is not None}
+        retVal['HASH'] = dict_hash(reqParams)
+        result_sets = jsonResp['resultSets']
         for result_set in result_sets:
             name = result_set['name']
             headers = result_set['headers']
@@ -33,7 +46,6 @@ def fetch_data(endpoint, params: dict):
             # Concat both tables ie:- Add req params as columns to the beginning of the table.
             retVal[name] = pandas.concat([paramsTable, pandaTable], axis=1)
     return retVal
-
 
 
 @ray.remote
